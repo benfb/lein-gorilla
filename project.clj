d662ad401b1f6618eaba55d42ebacdaeb1569ad6
Ben Bailey <bennettbailey@gmail.com>
2018-11-23 22:39:58 -0600

    version update

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.clojars.benfb/lein-gorilla "0.5.1"
+(defproject org.clojars.benfb/lein-gorilla "0.5.3"
   :description "A Leiningen plugin for the Gorilla REPL."
   :url "https://github.com/JonyEpsilon/lein-gorilla"
   :license {:name "MIT"}
-  :dependencies [[org.clojars.benfb/gorilla-repl "0.5.1"]]
+  :dependencies [[org.clojars.benfb/gorilla-repl "0.5.3"]]
   :eval-in-leiningen true)
